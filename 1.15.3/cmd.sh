64b49baa8ae6a25d731cd8ba143d7fadd10a6922
djin-cn <djin-cn@users.noreply.github.com>
2019-06-16 16:44:07 +0800

    修改acme安装方式和nginx配置方式

--- 1.15.3/cmd.sh
+++ 1.15.3/cmd.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+acmeFile="/root/.acme.sh/acme.sh"
+nginxHttpConf="/etc/nginx/conf.d/http/"
+
+if [ ! -f "$acmeFile" ]; then
+    echo 'install acme'
+    curl  https://get.acme.sh | sh \
+    && sed -i 's#> /dev/null#>> /root/.acme.sh/acme.log#' /var/spool/cron/crontabs/root
+fi
+
+if [ ! -d "$nginxHttpConf" ]; then
+    echo 'config nginx'
+    curl -fSL https://raw.githubusercontent.com/djin-cn/nginx/master/1.15.3/nginx.conf -o /etc/nginx/nginx.conf \
+    && curl -fSL https://raw.githubusercontent.com/djin-cn/nginx/master/1.15.3/nginx.vh.default.conf -o /etc/nginx/conf.d/http/default.conf \
+    && curl -fSL https://raw.githubusercontent.com/djin-cn/nginx/master/1.15.3/rtmp.conf -o /etc/nginx/conf.d/rtmp/rtmp.conf \
+    && curl -fSL https://raw.githubusercontent.com/djin-cn/nginx/master/1.15.3/ssl.conf -o /etc/nginx/conf.d/http/ssl.conf
+fi
+
 crond
 
 exec "$@"
